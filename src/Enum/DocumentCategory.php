7b81f72dca857a9a2895c8ee6a35cf097ed394ef
Joe Leigh <jleigh@illinois.edu>
2022-09-22 10:58:58 -0500

    Remove Offer Letter document option

--- src/Enum/DocumentCategory.php
+++ src/Enum/DocumentCategory.php
@@ -9,14 +9,12 @@ namespace App\Enum;
 enum DocumentCategory: string
 {
     case Certificate = 'certificate';
-    case OfferLetter = 'offer_letter';
     case Other = 'other';
 
     public function getChoiceLabel(): string
     {
         return match($this){
             DocumentCategory::Certificate => 'Certificate',
-            DocumentCategory::OfferLetter => 'Offer Letter',
             DocumentCategory::Other => 'Other',
         };
     }
