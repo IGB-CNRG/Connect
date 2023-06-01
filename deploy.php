b3d9da4cc4c92d0aa8d1738853592a9b8fad8d1b
Joe Leigh <jleigh@illinois.edu>
2023-06-01 15:23:59 -0500

    Add deployer recipe
    - Fix issue with liip imagine following symlinks to shared media folder

---
+++ deploy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony.php';
+require 'contrib/webpack_encore.php';
+
+// Config
+
+set('repository', 'git@github.com:IGBIllinois/CONNECT.git');
+
+add('shared_files', []);
+add('shared_dirs', ['public/media', 'public/uploads']);
+add('writable_dirs', ['public/media', 'public/uploads']);
+
+// Hosts
+import('deploy-hosts.yml');
+
+// Tasks
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+
+after('deploy:vendors', 'yarn:install');
+after('yarn:install', 'webpack_encore:build');
+before('deploy:symlink', 'database:migrate');
