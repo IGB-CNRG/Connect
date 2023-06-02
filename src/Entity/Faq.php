c6f880103878df50efa38d87a5b51681fb4400af
Joe Leigh <jleigh@illinois.edu>
2023-06-02 13:19:23 -0500

    Add FAQ entity

---
+++ src/Entity/Faq.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Copyright (c) 2023 University of Illinois Board of Trustees.
+ * All rights reserved.
+ */
+
+namespace App\Entity;
+
+use App\Repository\FaqRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FaqRepository::class)]
+class Faq
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $question = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $answer = null;
+
+    #[ORM\Column]
+    private ?int $position = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+}
